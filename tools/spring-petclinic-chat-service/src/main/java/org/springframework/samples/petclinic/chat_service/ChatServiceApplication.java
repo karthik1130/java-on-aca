dd6a201bebf56c0abbdfac36e0ee9a22073cde95
Karthikeya B <bairojukarthikeya@gmail.com>
2025-03-27 08:34:02 +0000

    init

--- tools/spring-petclinic-chat-service/src/main/java/org/springframework/samples/petclinic/chat_service/ChatServiceApplication.java
+++ tools/spring-petclinic-chat-service/src/main/java/org/springframework/samples/petclinic/chat_service/ChatServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ChatServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChatServiceApplication.class, args);
 	}
-
 }
